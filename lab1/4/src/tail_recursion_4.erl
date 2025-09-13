9002c0d460500a4a93b5b29b540ffa41b4d33d2b
nikzarch <razerr468@gmail.com>
2025-09-13 16:46:46 +0300

    tail recursion solution formatted

--- lab1/4/src/tail_recursion_4.erl
+++ lab1/4/src/tail_recursion_4.erl
@@ -2,7 +2,7 @@
 %%% @author nikzarch
 %%%
 %%% @doc
-%%% Module for finding the largest palindrome made from two 3-digits numbers.
+%%% Module for finding the largest palindrome made from the product of two 3-digits numbers.
 %%% @end
 %%%-------------------------------------------------------------------
 -module(tail_recursion_4).
@@ -12,11 +12,10 @@
 is_palindrome(Number) ->
   NumberList = integer_to_list(Number),
   is_palindrome_list(NumberList).
-is_palindrome_list([]) -> true;
 is_palindrome_list(List) ->
   List == lists:reverse(List).
 
-%%% Finds the largest palindrome made from two 3-digits numbers
+%% Finds the largest palindrome made from the product of two 3-digits numbers
 largest_palindrome_product_start() -> largest_palindrome_product(999, 999, 0).
 largest_palindrome_product(A, B, Max) when A == 99 ->
   largest_palindrome_product(999, B - 1, Max);
@@ -25,8 +24,12 @@ largest_palindrome_product(_, B, Max) when B == 99 ->
 largest_palindrome_product(A, B, Max) ->
   Number = A * B,
   case is_palindrome(Number) of
-    true -> if Number > Max -> largest_palindrome_product(A - 1, B, Number); true ->
-      largest_palindrome_product(A - 1, B, Max) end;
-    false -> largest_palindrome_product(A - 1, B, Max)
+    true ->
+      if Number > Max ->
+        largest_palindrome_product(A - 1, B, Number);
+        true ->
+          largest_palindrome_product(A - 1, B, Max)
+      end;
+    false ->
+      largest_palindrome_product(A - 1, B, Max)
   end.
-
