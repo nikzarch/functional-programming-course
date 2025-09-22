dbdce9b78bf38e8cd0432f8dce54b4bf29ef40cf
nikzarch <nikzarch@niuitmo.ru>
2025-09-22 23:09:14 +0300

    added tests

---
+++ lab1/task_4/test/task_4_tests.erl
@@ -0,0 +1,14 @@
+-module(task_4_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+test_modules_test() ->
+  Mods = [tail_recursion_4, regular_recursion_4, module_4, map_4, list_comprehension_4],
+  Expected = 906609,
+
+  TestFun = fun Test([Mod | Rest]) ->
+    Result = Mod:largest_palindrome_product(),
+    ?assertEqual(Expected, Result),
+    Test(Rest);
+    Test([]) -> ok
+            end,
+  TestFun(Mods).
