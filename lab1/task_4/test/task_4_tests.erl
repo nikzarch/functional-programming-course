dfe308393706914d580ee715cac6dece8cba37ef
nikzarch <nikzarch@niuitmo.ru>
2025-09-24 00:10:07 +0300

    formatted test

--- lab1/task_4/test/task_4_tests.erl
+++ lab1/task_4/test/task_4_tests.erl
@@ -2,13 +2,15 @@
 -include_lib("eunit/include/eunit.hrl").
 
 test_modules_test() ->
-  Mods = [tail_recursion_4, regular_recursion_4, module_4, map_4, list_comprehension_4],
-  Expected = 906609,
+    Mods = [tail_recursion_4, regular_recursion_4, module_4, map_4, list_comprehension_4],
+    Expected = 906609,
 
-  TestFun = fun Test([Mod | Rest]) ->
-    Result = Mod:largest_palindrome_product(),
-    ?assertEqual(Expected, Result),
-    Test(Rest);
-    Test([]) -> ok
-            end,
-  TestFun(Mods).
+    TestFun = fun
+        Test([Mod | Rest]) ->
+            Result = Mod:largest_palindrome_product(),
+            ?assertEqual(Expected, Result),
+            Test(Rest);
+        Test([]) ->
+            ok
+    end,
+    TestFun(Mods).
